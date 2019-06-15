bd637c15394a88df8477513b4ef17716f7e30ae1
范帅兵 <ail>
2019-06-15 18:55:22 +0800

    观察者设计模式

---
+++ desianmodel/com/observermodel/Subject.java
@@ -0,0 +1,10 @@
+package com.observermodel;
+
+public interface Subject {
+
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+
+    //其他的操作
+}
---
+++ desianmodel/com/observermodel_2/DisplayElement.java
@@ -0,0 +1,5 @@
+package com.observermodel_2;
+
+public interface DisplayElement {
+    public void display();
+}
